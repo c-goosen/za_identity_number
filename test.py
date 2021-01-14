2b95f6aade43ef43ac02935f8c5630be1fbcfcdc
Christo Goosen <christogoosen@gmail.com>
2021-01-14 18:15:07 +0200

    Fix test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-from za_id_number import SouthAfricanValidation
+from za_id_number.za_identity_number import SouthAfricanIdentityValidate
 
-validate_id = SouthAfricanValidation()
+validate_id = SouthAfricanIdentityValidate('9001245289086')
 
 if __name__ == "__main__":
-    print(validate_id.identity_dict('9001245289086'))
+    print(validate_id.identity())
